8d38df9c8e48e153a463ef658b674a5285f6ddc0
Bradon-Lodwick <bradonlodwick22@gmail.com>
2017-10-28 23:13:30 -0400

    Create Database API File
    Added a file to be used for connecting to and manipulating the database

--- Database.py
+++ Database.py
@@ -8,3 +8,20 @@ with open('Config.yaml') as config_yml:
 host = cfg['database']['host']  # The host address for the database
 user = cfg['database']['user']  # The username for the database
 password = cfg['database']['password']  # The password for the database
+name = cfg['database']['name']  # The name of the database
+
+# Connection to the database using config settings
+try:
+    conn_string = "dbname='" + name + "' user='" + user + "' host='" + host + "' password='" + password + "'"
+    conn = psycopg2.connect(conn_string)
+except psycopg2.Error as e:
+    print(e)
+
+# Test sql statement
+try:
+    sql = "SELECT * FROM players"
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+except psycopg2.Error as e:
+    print(e)
