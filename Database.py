4a3e290a3a9ada34a735d564d2b634f3a6ea33a3
Bradon-Lodwick <bradonlodwick22@gmail.com>
2017-11-01 19:10:21 -0400

    Added Database Commands
    Added commands to add players and set their team in the Database.py file

--- Database.py
+++ Database.py
@@ -44,17 +44,70 @@ def add_player(server, discord, ign):
     :param server: The discord server id that the player is registering in.
     :param discord: The discord id of the player that is registering.
     :param ign: The in-game-name of the player that is registering.
-    :return:
+    :return: Returns whether the player was added successfully to the database via boolean.
     """
 
     # Accesses the global DB variable
     global DB
 
-    # The sql to be executed, using placeholders for the values to be inserted
-    sql = """INSERT INTO "Players"(server, discord, ign) VALUES ('%s', '%s', '%s');"""
+    # Tries to add the player into the database
+    try:
+        # The sql to be executed, using placeholders for the values to be inserted
+        sql = """INSERT INTO "Players"(server, discord, ign) VALUES (%s, %s, %s);"""
+        # Cursor to execute the sql insertion
+        cur = DB.cursor()
+        # Executes the sql, filling the placeholders with the arguments
+        cur.execute(sql, (server, discord, ign))
+        # Commits the execution
+        DB.commit()
+        # Close the cursor
+        cur.close()
+        # Returns True to signal the connection was successful
+        return True
+    # Excepts any errors generated while adding the player to the database
+    except psycopg2.Error as e:
+        # Logs the errors
+        logging.error(" {}\n{}".format(datetime.datetime.now(), e))
+        # Returns False to signal adding the player was unsuccessful
+        return False
 
-    # Executes the sql, filling the placeholders with the arguments
-    DB.execute(sql, (server, discord, ign))
 
+def set_team(server, player, team):
+    """ Sets a player's team in the database.
 
-connect()
+    :param server: The dicsord server id of the player.
+    :param player: The discord id of the player to have their team set to.
+    :param team: The discord id of the team to have the player set to.
+    :return: Returns whether setting the team of the player was successful via boolean.
+    """
+
+    # Accesses the global DB variable
+    global DB
+
+    # Tries to change the team of the player in the database
+    try:
+        # The sql to be executed, using placeholders for the values to be inserted
+        sql = """Update "Players" p
+            SET team = t.id
+            FROM "Teams" t
+            WHERE
+                p.server = %s AND
+                t.server = p.server AND
+                p.discord = %s AND
+                t.discord = %s;"""
+        # Cursor to execute the sql insertion
+        cur = DB.cursor()
+        # Executes the sql that changes the player's team
+        cur.execute(sql, (server, player, team))
+        # Commits the execution
+        DB.commit()
+        # Close the cursor
+        cur.close()
+        # Returns True to signal changing the player's team was successful
+        return True
+    # Excepts any errors generated while changing the player's team in the database
+    except psycopg2.Error as e:
+        # Logs the errors
+        logging.error(" {}\n{}".format(datetime.datetime.now(), e))
+        # Returns False to signal changing the player's team was unsuccessful
+        return False
