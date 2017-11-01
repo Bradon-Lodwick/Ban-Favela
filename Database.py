c0f52e95c29d65e0b33e472947ac28c702e69b1a
Bradon-Lodwick <bradonlodwick22@gmail.com>
2017-11-01 12:41:49 -0400

    Working Commit

--- Database.py
+++ Database.py
@@ -1,20 +1,40 @@
 import psycopg2  # Used to connect to the postgresql database
-import settings  # Holds necessary globals such as database connection strings obtained from the Config.yaml file
+import Settings  # Holds necessary globals such as database connection strings obtained from the Config.yaml file
 import logging  # Used to log errors and values in the log file
-import datetime  # Used to get the date and time
+import datetime  # Used to log times of errors
+
+
+# Holds the database connection after running connect()
+DB = None
+
+# Sets up error logging
+logging.basicConfig(filename=Settings.LOG, level=logging.ERROR)
+
 
 def connect():
     """ Used to open the database connection. Returns whether or not the connection was opened successfully.
     Prints the connection error if an error was occurred.
+
     :return: Returns whether the database connection was opened successfully via boolean.
     """
+
+    # Accesses the global DB variable
+    global DB
+
+    # Tries to access the database
     try:
+        # The string used to connect to the database
         conn_string = """dbname={} user={} host={} password={}""".format(
-        settings.DB_NAME, settings.DB_USER, settings.DB_HOST, settings.DB_PASS)
-        conn = psycopg2.connect(conn_string)
+            Settings.DB_NAME, Settings.DB_USER, Settings.DB_HOST, Settings.DB_PASS)
+        # Connects to the database
+        DB = psycopg2.connect(conn_string)
+        # Returns True to signal the connection was successful
         return True
+    # Excepts any errors generated while connecting to the database for logging purposes
     except psycopg2.Error as e:
-        print(e)
+        # Logs the errors
+        logging.error(" {}\n{}".format(datetime.datetime.now(), e))
+        # Returns False to signal the connection was unsuccessful
         return False
 
 
@@ -27,19 +47,14 @@ def add_player(server, discord, ign):
     :return:
     """
 
+    # Accesses the global DB variable
+    global DB
+
     # The sql to be executed, using placeholders for the values to be inserted
     sql = """INSERT INTO "Players"(server, discord, ign) VALUES ('%s', '%s', '%s');"""
 
     # Executes the sql, filling the placeholders with the arguments
-    conn.execute(sql, (server, discord, ign))
+    DB.execute(sql, (server, discord, ign))
 
-# Test sql statement
-try:
-    sql = """SELECT * FROM "Teams";"""
-    cur = conn.cursor()
-    cur.execute(sql)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-except psycopg2.Error as e:
-    print(e)
+
+connect()
