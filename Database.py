48c9cb060d64961c0ec6181640eda222ac170242
Bradon-Lodwick <bradonlodwick22@gmail.com>
2017-11-02 22:55:53 -0400

    Working Commit
    TODO: Fix a todo in the Database.py file

--- Database.py
+++ Database.py
@@ -1,113 +1,172 @@
+"""
+todo: don't think I can have Settings.py and Database.py import each other
+"""
+
 import psycopg2  # Used to connect to the postgresql database
 import Settings  # Holds necessary globals such as database connection strings obtained from the Config.yaml file
 import logging  # Used to log errors and values in the log file
 import datetime  # Used to log times of errors
-
-
-# Holds the database connection after running connect()
-DB = None
+from contextlib import contextmanager  # Used to make connecting to the database and logging errors easier
 
 # Sets up error logging
-logging.basicConfig(filename=Settings.LOG, level=logging.ERROR)
+logging.basicConfig(filename=Settings.LOG, level=logging.ERROR)  # Sets errors to be written to
 
 
-def connect():
-    """ Used to open the database connection. Returns whether or not the connection was opened successfully.
-    Prints the connection error if an error was occurred.
+@contextmanager
+def connect(conn_string=Settings.DB_CONN_STRING, commit=False):
+    """ Used to connect to the database. Use the cursor object to do any queries.
 
-    :return: Returns whether the database connection was opened successfully via boolean.
+    :param conn_string: The string to be used to connect to the database.
+    Should specify the databases name, user, password, and host address.
+    Defaults to the DB_CONN_STRING in the Settings.py file.
+    :type conn_string: str
+
+    :param commit: Used to say if the cursor is to be committed to the database.
+    Defaults to False so accidental commits are avoided.
+    :type commit: bool
     """
 
-    # Accesses the global DB variable
-    global DB
-
-    # Tries to access the database
     try:
-        # The string used to connect to the database
-        conn_string = """dbname={} user={} host={} password={}""".format(
-            Settings.DB_NAME, Settings.DB_USER, Settings.DB_HOST, Settings.DB_PASS)
         # Connects to the database
-        DB = psycopg2.connect(conn_string)
-        # Returns True to signal the connection was successful
-        return True
-    # Excepts any errors generated while connecting to the database for logging purposes
-    except psycopg2.Error as e:
-        # Logs the errors
-        logging.error(" {}\n{}".format(datetime.datetime.now(), e))
-        # Returns False to signal the connection was unsuccessful
-        return False
+        db = psycopg2.connect(conn_string)
+        # Cursor to be used when interacting with the database
+        cursor = db.cursor()
+        # Yield the cursor so it can be used in other functions
+        yield cursor
+    # Excepts any database errors so they can be handled and logged
+    except psycopg2.DatabaseError as e:
+        # Gets the argument variables
+        error, = e.args
+        # Logs the error and the time of the error
+        logging.error(" {}\n{}".format(error.message, datetime.datetime.now))
+        # Changes the return_val to False so False will be returned in the finally clause.
+        return_val = False
+    # If an error doesn't occur, determines whether a commit is to be made
+    else:
+        # If a commit was to be made
+        if commit:
+            # Commits the database
+            db.commit()
+        # If a commit wasn't to be made
+        else:
+            # Rolls the database back
+            db.rollback()
+    # Finally the database connection will be closed
+    finally:
+        # Closes the database
+        db.close()
+
+
+def get_settings():
+    """ Gets the server settings from the database.
+
+    :return: Returns the server settings
+    :rtype: dict
+    """
+
+    # Opens a database connection where commits cannot occur, with a cursor object to use in that connection
+    with connect() as cursor:
+        # The SQL code to get all of the server settings that are in the Settings table of the database.
+        sql = """SELECT server, c_symbol, game_type, current_season, current_round, admin_role, team_size
+            FROM "Settings";"""
+        # Executes the sql query with the given parameters
+        cursor.execute(sql)
+        # Gets all of the rows obtained from the sql query
+        rows = cursor.fetchall()
+        # The settings dictionary
+        server_settings = dict()
+        # Loops through all of the rows in the query
+        for row in rows:
+            # The server id to be used as the key in the dictionary
+            id = row[0]
+            # The sub-dictionary that will hold all the settings for the current server
+            sub_dictionary = dict()
+            # Assigns the command symbol of the server
+            sub_dictionary['c_symbol'] = row[1]
+            # Assigns the game type of the server
+            sub_dictionary['game_type'] = row[2]
+            # Assigns the current season of the server
+            sub_dictionary['current_season'] = row[3]
+            # Assigns the current round of the server
+            sub_dictionary['current_round'] = row[4]
+            # Assigns the admin role of the server
+            sub_dictionary['admin_role'] = row[5]
+            # Assigns the team size of the server
+            sub_dictionary['team_size'] = row[6]
+            # Assigns the settings of the server into the server_settings dictionary using the server's discord id as
+            # they key
+            server_settings[id] = sub_dictionary
+        # Returns the server settings dictionary
+        return server_settings
+
+
+def update_settings(server, setting_type, setting_val):
+    """ Updates the server settings in the Settings.py file as well as the database.
+
+    :param server: The discord server id of the server to be added.
+    :type server: str
+
+    :param setting_type: The setting to be changed in the server.
+    :type setting_type: str
+
+    :param setting_val: The value to have the setting changed to.
+    :type setting_val: int, str, etc
+    """
+    # Opens a database connection where commits can occur, with a cursor object to use in that connection
+    # to update the setting in the database
+    with connect(commit=True) as cursor:
+        # The sql to be executed, using placeholders for the values to be inserted
+        sql = """Update "Settings" SET %s = $s WHERE p.server = %s"""
+        # Executes the sql query with the given parameters
+        cursor.execute(sql, (setting_type, setting_val, server))
+
+    # Changes the value of the setting in the Settings.py dictionary
+    Settings.SERVER_SETTINGS[server][setting_type] = setting_val
 
 
 def add_player(server, discord, ign):
-    """ Adds a new player to the database.
+    """ Adds a player to the database. Uses the cursor object to do any queries.
 
-    :param server: The discord server id that the player is registering in.
-    :param discord: The discord id of the player that is registering.
-    :param ign: The in-game-name of the player that is registering.
-    :return: Returns whether the player was added successfully to the database via boolean.
+    :param server: The discord server id of the server to have the player added into.
+    :type server: str
+
+    :param discord: The discord id of the player to be added.
+    :type discord: str
+
+    :param ign: The in-game-name of the player to be added.
+    :type ign: str
     """
 
-    # Accesses the global DB variable
-    global DB
-
-    # Tries to add the player into the database
-    try:
-        # The sql to be executed, using placeholders for the values to be inserted
+    # Opens a database connection where commits can occur, with a cursor object to use in that connection
+    with connect(commit=True) as cursor:
+        # SQL code to insert the new player into the database without assigning them a team
         sql = """INSERT INTO "Players"(server, discord, ign) VALUES (%s, %s, %s);"""
-        # Cursor to execute the sql insertion
-        cur = DB.cursor()
-        # Executes the sql, filling the placeholders with the arguments
-        cur.execute(sql, (server, discord, ign))
-        # Commits the execution
-        DB.commit()
-        # Close the cursor
-        cur.close()
-        # Returns True to signal the connection was successful
-        return True
-    # Excepts any errors generated while adding the player to the database
-    except psycopg2.Error as e:
-        # Logs the errors
-        logging.error(" {}\n{}".format(datetime.datetime.now(), e))
-        # Returns False to signal adding the player was unsuccessful
-        return False
+        # Executes the sql query with the given parameters
+        cursor.execute(sql, (server, discord, ign))
 
 
 def set_team(server, player, team):
     """ Sets a player's team in the database.
 
-    :param server: The dicsord server id of the player.
+    :param server: The discord server id of the player.
+    :type server: str
+
     :param player: The discord id of the player to have their team set to.
+    :type player: str
+
     :param team: The discord id of the team to have the player set to.
-    :return: Returns whether setting the team of the player was successful via boolean.
+    :type team: str
     """
 
-    # Accesses the global DB variable
-    global DB
-
-    # Tries to change the team of the player in the database
-    try:
+    # Opens a database connection where commits can occur, with a cursor object to use in that connection
+    with connect(commit=True) as cursor:
         # The sql to be executed, using placeholders for the values to be inserted
-        sql = """Update "Players" p
-            SET team = t.id
-            FROM "Teams" t
-            WHERE
-                p.server = %s AND
-                t.server = p.server AND
-                p.discord = %s AND
-                t.discord = %s;"""
-        # Cursor to execute the sql insertion
-        cur = DB.cursor()
-        # Executes the sql that changes the player's team
-        cur.execute(sql, (server, player, team))
-        # Commits the execution
-        DB.commit()
-        # Close the cursor
-        cur.close()
-        # Returns True to signal changing the player's team was successful
-        return True
-    # Excepts any errors generated while changing the player's team in the database
-    except psycopg2.Error as e:
-        # Logs the errors
-        logging.error(" {}\n{}".format(datetime.datetime.now(), e))
-        # Returns False to signal changing the player's team was unsuccessful
-        return False
+        sql = """Update "Players" p SET team = t.id FROM "Teams" t 
+        WHERE p.server = %s AND t.server = p.server AND p.discord = %s AND t.discord = %s;"""
+        # Executes the sql query with the given parameters
+        cursor.execute(sql, (server, player, team))
+
+
+#get_settings()
+#update_settings('360201172851621890', 'c_symbol', '@')
+print('temp')
--- Settings.py
+++ Settings.py
@@ -1,17 +1,25 @@
 import yaml  # Used to load the settings from the Config.yaml file
+import Database  # Used to load the discord server settings into a dictionary from the database
 
 # Load the config file into a dictionary
 with open("""Config.yaml""") as cfg_file:
     cfg = yaml.load(cfg_file)
 
-# Loads all of the database information
+# Loads all of the database information from the config file
 DB_HOST = cfg['database']['host']  # The host address of the database
 DB_USER = cfg['database']['user']  # The user of the database
 DB_PASS = cfg['database']['password']  # The password of the user connecting to the database
 DB_NAME = cfg['database']['name']  # The name of the database being connected to
+# The string to be used to connect to the postgres database
+DB_CONN_STRING = """dbname={} user={} host={} password={}""".format(
+    DB_NAME, DB_USER, DB_HOST, DB_PASS
+)
 
-# Loads the discord bot token
+# Loads the discord bot token from the config file
 TOKEN = cfg['token']
 
-# The location for the log file
+# The location for the log file loaded from the config file
 LOG = cfg['log']
+
+# Loads the discord server settings from the database
+SERVER_SETTINGS = Database.get_settings()
